569b0b50653bf3226580b0781cb74e103be07b00
MNelson <melissa.nelson13@yahoo.com>
2018-10-03 21:08:24 -0500

    added moment.js to calculate next train arrival and minutes to next train

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Train-Scheduler
+
+### Link to Deployed Project:
+* https://mnelson13.github.io/Train-Scheduler/
+
+### Instructions:
+* use the form to add a train to the train list
+
+* the next arrival and minutes away will automatically calculate
+
+* your train will be saved across browsers
+
+### Languages/Technologies Used:
+* HTML, CSS, Bootstrap, JavaScript, jQuery, Firebase, moment.js
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,18 +19,40 @@ var config = {
   var nextArrival = '';
   var minutesAway = '';
 
+
+  //stores new train info from form on Firebase when submit is clicked
   $("#add-train").on("click", function(event){
       event.preventDefault();
 
+      var currentTime = moment();
+
+      //stores train info from the form into variables
       name = $("#name-input").val().trim();
       destination = $("#destination-input").val().trim();
       frequency = $("#frequency-input").val().trim();
       firstTrainTime = $("#time-input").val().trim();
 
-      //code needed here to convert firstTrainTime into nextArrival and minutesAway??
-        nextArrival = "TBD";
-        minutesAway = "TBD";
 
+      var firstTrainTimeConverted = moment(firstTrainTime, "HH:mm");
+
+      //if statement with calculations to get minutesAway and nextArrival
+      if (firstTrainTimeConverted < currentTime) {
+        var diffTime = currentTime.diff(moment(firstTrainTimeConverted), "minutes");
+
+        var tRemainder = diffTime % frequency;
+
+        minutesAway = frequency - tRemainder;
+
+        nextArrival = currentTime.add(minutesAway, "minutes");
+        nextArrival = moment(nextArrival).format("HH:mm");
+
+      } else if (firstTrainTimeConverted >= currentTime) {
+        minutesAway = firstTrainTimeConverted.diff(moment(currentTime), "minutes");
+
+        nextArrival = firstTrainTime;
+      }
+
+      //pushes new train onto database
       database.ref().push({
         name: name,
         destination: destination,
@@ -39,8 +61,16 @@ var config = {
         minutesAway: minutesAway,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
       });
+
+      //clears the form
+      $("#name-input").val("");
+      $("#destination-input").val("");
+      $("#frequency-input").val("");
+      $("#time-input").val("");
+
   })
 
+  //updates the page on reload and appends to table row when a new train is added
   database.ref().on("child_added", function(childSnapshot) {
 
     $("#train-table").append(
